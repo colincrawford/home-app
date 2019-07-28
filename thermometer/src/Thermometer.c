f17e897ab557fba4c6873f2b3cb32815d3c53074
colinlcrawford <ccolin84@gmail.com>
2019-07-28 15:37:46 -0400

    refactor temperatures to struct and change timestring logic

--- thermometer/src/Thermometer.c
+++ thermometer/src/Thermometer.c
@@ -14,12 +14,45 @@
 #define A3 pinbase + 3
 #define DELAY 2000
 
+struct temperature_t
+{
+	float tempKelvin;
+	float tempCelsius;
+	float tempFahrenheit;
+};
+
+void temperature(struct temperature_t *temp, float resistance)
+{
+	float tempKelvin = kelvin_temp_from_resistance(resistance);
+	float tempCelsius = celsius_from_kelvin(tempKelvin);
+	float tempFahrenheit = fahrenheit_from_celsius(tempCelsius);
+	temp->tempKelvin = tempKelvin;
+	temp->tempCelsius = tempCelsius;
+	temp->tempFahrenheit = tempFahrenheit;
+}
+
+void set_current_time(char *time_str, struct tm *current_time)
+{
+	int year, month, day, hour, minute, second;
+	year = current_time->tm_year + 1900;
+	month = current_time->tm_mon + 1;
+	day = current_time->tm_mday;
+	hour = current_time->tm_hour;
+	minute = current_time->tm_min;
+	second = current_time->tm_sec;
+	memset(time_str, 0, sizeof(char) * 100);
+	sprintf(time_str, "%02d/%02d/%02d %02d:%02d:%02d", day, month, year, hour, minute, second);
+}
+
 int main(void)
 {
 	int adcValue;
 	float tempKelvin, tempCelsius, tempFahrenheit;
 	float voltage, resistance;
 	time_t t;
+	struct tm *current_time;
+	struct temperature_t temp;
+	char time_str[100];
 
 	// exit when setting up the wire fails
 	if (wiringPiSetup() == -1)
@@ -32,18 +65,21 @@ int main(void)
 
 	while (true)
 	{
-		time(&t);
+		// time in seconds
+		t = time(NULL);
+		// to get current time
+		current_time = localtime(&t);
+		set_current_time(time_str, current_time);
+
 		//read A0 pin
 		adcValue = analogRead(A0);
 		// calculate voltage
 		voltage = voltage_from_pin(adcValue);
 		resistance = resistance_from_voltage(voltage);
-		tempKelvin = kelvin_temp_from_resistance(resistance);
-		tempCelsius = celsius_from_kelvin(tempKelvin);
-		tempFahrenheit = fahrenheit_from_celsius(tempCelsius);
+		temperature(&temp, resistance);
 		printf(
 				"%s - ADC value : %d  ,\tVoltage : %.2fV, \tTemperature : %.2fC %.2fF\n",
-				ctime(&t), adcValue, voltage, tempCelsius, tempFahrenheit);
+				time_str, adcValue, voltage, temp.tempCelsius, temp.tempFahrenheit);
 		delay(DELAY);
 	}
 	return 0;
