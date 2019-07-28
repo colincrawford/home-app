6e7221f571b0d517ff39903c54d5b6dcadce42a4
colinlcrawford <ccolin84@gmail.com>
2019-07-27 22:02:08 -0400

    add missing float kelvin_temp_from_resistance to conversions header

--- thermometer/src/conversions.h
+++ thermometer/src/conversions.h
@@ -1,4 +1,5 @@
 float voltage_from_pin(int pinValue);
+float kelvin_temp_from_resistance(float resistance);
 float resistance_from_voltage(float voltage);
 float celsius_from_kelvin(float tempKelvin);
 float fahrenheit_from_celsius(float tempCelsius);
