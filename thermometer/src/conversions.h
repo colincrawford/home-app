e6d98016813b0bffe7c7a99518ad05cc4a05a98b
colinlcrawford <ccolin84@gmail.com>
2019-07-29 21:24:58 -0400

    add include guards to header files

--- thermometer/src/conversions.h
+++ thermometer/src/conversions.h
@@ -1,5 +1,10 @@
+#ifndef CONVERSIONS_H
+#define CONVERSIONS_H
+
 float voltage_from_pin(int pinValue);
 float kelvin_temp_from_resistance(float resistance);
 float resistance_from_voltage(float voltage);
 float celsius_from_kelvin(float tempKelvin);
 float fahrenheit_from_celsius(float tempCelsius);
+
+#endif
--- thermometer/src/temperature.h
+++ thermometer/src/temperature.h
@@ -1,3 +1,6 @@
+#ifndef TEMPERATURE_H
+#define TEMPERATURE_H
+
 typedef struct {
 	float kelvin;
 	float celsius;
@@ -5,3 +8,5 @@ typedef struct {
 } temperature;
 
 void init_temperature(temperature *temp, float resistance);
+
+#endif
