1d1ebccbe66fb16323e9e661776d51fc1ee7e346
colinlcrawford <ccolin84@gmail.com>
2019-08-07 22:22:50 -0400

    add script to download rabbit-mq-c library

---
+++ thermometer/send-to-rabbitmq/download-rabbit-mq-c.sh
@@ -0,0 +1,7 @@
+curl -L -o rabbitmq-c-0.9.0.tar.gz https://github.com/alanxz/rabbitmq-c/archive/v0.9.0.tar.gz \
+    && tar xvzf rabbitmq-c-0.9.0.tar.gz \
+    && rm rabbitmq-c-0.9.0.tar.gz \
+    && cd rabbitmq-c-0.9.0 \
+    && mkdir build && cd build \
+    && cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/ -DBUILD_STATIC_LIBS=ON .. \
+    && cmake --build . --target install
