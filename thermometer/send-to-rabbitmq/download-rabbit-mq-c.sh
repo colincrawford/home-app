e1b6d27ea52aece58136e92c9d0d49b946d0c8fc
colinlcrawford <ccolin84@gmail.com>
2019-08-11 19:22:26 -0400

    add shebang to bash scripts

--- thermometer/send-to-rabbitmq/download-rabbit-mq-c.sh
+++ thermometer/send-to-rabbitmq/download-rabbit-mq-c.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -L -o rabbitmq-c-0.9.0.tar.gz https://github.com/alanxz/rabbitmq-c/archive/v0.9.0.tar.gz \
     && tar xvzf rabbitmq-c-0.9.0.tar.gz \
     && rm rabbitmq-c-0.9.0.tar.gz \
--- thermometer/thermometer/download-check.sh
+++ thermometer/thermometer/download-check.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -L -o check-0.12.0.tar.gz https://github.com/libcheck/check/releases/download/0.12.0/check-0.12.0.tar.gz
 
 tar xvzf check-0.12.0.tar.gz
